74c97c03bee57684f0c47fa9d782e9be10dedbc5
VINKOS <kravter7@gmail.com>
2024-05-15 15:34:32 +0300

    feat: auth-update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { SignInPage } from './pages/SignInPage/SignInPage';
-import { SignUpPage } from './pages/SignUpPage/SignUpPage';
+import { AuthPage } from './pages/Auth/AuthPage';
 import { MainPage } from './pages/MainPage/MainPage';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const footer = useSelector((state: any) => state.footer)
+  const viewApp = useSelector((state: any) => state.viewApp)
+
   return (
-    <div className="App">
+    <div className={`App ${viewApp.scrollX? '': 'blockScrollX'} ${viewApp.scrollY? '': 'blockScrollY'}`}>
       <BrowserRouter> 
         <Routes>
         <Route path='/' element={<MainPage/>}/>
-          <Route path='/signin' element={<SignInPage/>}/>
-          <Route path='/signup' element={<SignUpPage/>}/>
+          <Route path='/signin' element={<AuthPage/>}/>
         </Routes>
       </BrowserRouter> 
       <div className='header'>
-        <div className="discriptionWhoDevelopment">
-                  Разработано департаментом цифровых систем управления и технологии АО "НТЦ ФСК ЕЭС"®
-        </div>    
+        {footer.show && footer.value} 
       </div>
     </div>
   );
