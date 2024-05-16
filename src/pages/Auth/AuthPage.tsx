654470895fae8a2a2a72e03492ae516367e6ba5d
VINKOS <kravter7@gmail.com>
2024-05-16 12:44:26 +0300

    feat: recovery password

--- src/pages/Auth/AuthPage.tsx
+++ src/pages/Auth/AuthPage.tsx
@@ -34,6 +34,7 @@ export const AuthPage = () => {
     const [isSignUp, setIsSignUp] = useState(false)
     const [rejectWindow, setRejectWindow] = useState(false)
     const [acceptWindow, setAcceptWindow] = useState(false)
+    const [recoveryPasswordWindow, setRecoveryPasswordWindow] = useState('')
 
     const footer = useRef(useSelector((state: any) => state.footer))
     const email = useRef('')
@@ -56,8 +57,25 @@ export const AuthPage = () => {
         }, 15000)
     }, [acceptWindow])
 
+    useEffect(() => {
+        if(recoveryPasswordWindow === 'message') setTimeout(() => {
+            setRecoveryPasswordWindow('')
+            dispatch(setFooter({show: true, value: <Footer slot={getFooter()}/>}))
+        }, 10000)
+    }, [recoveryPasswordWindow])
+
     const closeAcceptWindow = () => {
         setAcceptWindow(false)
+        dispatch(setFooter({show: true, value: <Footer slot={getFooter()}/>}))
+    }
+
+    const acceptRecoveryPasswordWindow  = () => {
+        setRecoveryPasswordWindow('message')
+        dispatch(setFooter({show: true, value: <Footer slot={getFooter()}/>}))
+    }
+
+    const closeRecoveryPasswordWindow = () => {
+        setRecoveryPasswordWindow('')
         dispatch(setFooter({show: true, value: <Footer slot={getFooter()}/>}))
     }
     
@@ -86,6 +104,12 @@ export const AuthPage = () => {
                 dispatch(setFooter({show: true, value: <Footer slot={getFooter()}/>}))
                 break
         }       
+    }
+
+    const onClickRecovery = () => {
+        dispatch(setFooter({...footer.current, ['show']: false}))
+
+        setRecoveryPasswordWindow('label')
     }
 
     return(
@@ -117,7 +141,7 @@ export const AuthPage = () => {
                                     <ErrorMessage name="email" component="div" />
                                     <Field type="password" name="password" placeholder="password"/>
                                     <ErrorMessage name="password" component="div" />
-                                    <Link to={"/"} className={styles.fogotPassword}>Восстановить пароль</Link>
+                                    <div onClick={onClickRecovery} className={styles.fogotPassword}>Восстановить пароль</div>
                                     <button className={styles.signInButton} type="submit" disabled={isSubmitting}>Войти</button>
                                     <button className={styles.signUpButton} onClick={onClickToSignUp}>Зарегистрироваться</button>
                                 </Form>
@@ -152,6 +176,32 @@ export const AuthPage = () => {
                             supportftcsoftware@ftc-energo.ru
                         </a>
                         <br/><button onClick={closeAcceptWindow} className={styles.button}>OK</button>
+                    </div>
+                </div>
+            }
+            {recoveryPasswordWindow === 'label' &&
+                <div className={styles.recoveryPasswordWindow}>
+                    <button onClick={closeRecoveryPasswordWindow} className={styles.closeButton}>Х</button>
+                    <h3>Восcтановление пароля</h3>
+                    <h4 className={styles.recEmail}>Email</h4>
+                    <input>
+                    </input>
+                    <br/><button onClick={acceptRecoveryPasswordWindow} className={styles.button}>OK</button>
+                </div>
+            }
+            {recoveryPasswordWindow === 'message' &&
+                <div className={styles.recoveryPasswordWindowMessage}>
+                    <button onClick={closeAcceptWindow} className={styles.closeButton}>Х</button>
+                    <h4>Восcтановление пароля</h4>
+                    <div>
+                        На адрес {email.current} отправлено письмо с инструкцией по установке пароля. 
+                        Пожалуйста, выполните инструкции в письме для восстановления пароля. 
+                        <br/>-<br/>
+                        Если письмо не пришло, проверьте папку “Спам” или обратитесь в службу технической поддержки&nbsp; 
+                        <a  href= "mialto: supportftcsoftware@ftc-energo.ru" className={styles.emailSupport}>
+                            supportftcsoftware@ftc-energo.ru
+                        </a>
+                        <br/><button onClick={closeRecoveryPasswordWindow} className={styles.button}>OK</button>
                     </div>
                 </div>
             }
